22414850f8abc9b2ef6f6f9d83354dfced4025fe
09SethT@gmail.com <09SethT@gmail.com@e7c1a69f-48f6-0573-76fe-9e04cafba95b>
2011-10-07 20:08:17 +0000

    Adding disable interupts to thread_set_priority.

--- trunk/threads/thread.c
+++ trunk/threads/thread.c
@@ -399,10 +399,11 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->base_priority = new_priority;
+	enum intr_level old_level = intr_disable();  
+	thread_current ()->base_priority = new_priority;
 	if (!thread_current()->in_donation)
 		thread_current()->priority = new_priority;
-	
+	intr_set_level(old_level);
 	thread_yield_to_higher_priority();
 }
 
