3273c91b4dd394d84475e7cc19d439a151bf9fe8
09SethT@gmail.com <09SethT@gmail.com@e7c1a69f-48f6-0573-76fe-9e04cafba95b>
2011-10-17 00:15:43 +0000

    some changes -- doesn't run/compile... incomplete

--- trunk/userprog/syscall.c
+++ trunk/userprog/syscall.c
@@ -12,6 +12,47 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+void
+hault(void)
+{
+
+}
+
+void
+exit(int status)
+{
+
+}
+
+pid_t 
+exec(const char *cmd_line)
+{
+
+}
+
+int wait(pid_t pid)
+{
+
+}
+
+bool 
+create(const* file, unsigned initial_size)
+{
+
+}
+
+bool
+remove(const char*file)
+{
+
+}
+
+int 
+open(const char *file)
+{
+
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
