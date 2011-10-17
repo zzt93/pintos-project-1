13b9a6643219d27dcf8b4abf8d1313312cfacfcd
09SethT@gmail.com <09SethT@gmail.com@e7c1a69f-48f6-0573-76fe-9e04cafba95b>
2011-10-17 17:53:32 +0000

    Adding print lines to process functions

--- trunk/userprog/process.c
+++ trunk/userprog/process.c
@@ -28,6 +28,9 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
 tid_t
 process_execute (const char *file_name) 
 {
+  printf("beginning.....\n");
+  printf("file is %s\n", file_name);
+
   char *fn_copy;
   tid_t tid;
 
@@ -37,9 +40,12 @@ process_execute (const char *file_name)
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
+  printf("fn_copy: %s\n", fn_copy);
   
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  printf("tid is %d\n", tid);
+  
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy);
   return tid;
@@ -67,13 +73,20 @@ int get_count(char* s)
 static void
 start_process (void *file_name_)
 {
+  printf("starting process.....\n");
   char *fn_copy;
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
     thread_exit ();//This might be awful and not work
   strlcpy (fn_copy, (char*)file_name_, PGSIZE);  
-
+  
+  // print fn_copy
+  printf("fn_copy: %s\n", fn_copy);
+  
   int c = get_count(fn_copy);
+
+  printf("c: %d\n", c);  
+
   char** v = malloc(sizeof(char*)*c);
   
   char *token, *save_ptr;
@@ -83,8 +96,10 @@ start_process (void *file_name_)
   {
      v[i] = malloc(sizeof(char)*strlen(token));
      strlcpy(token, v[i], strlen(token));
+     printf("token found[%d]: %s\n", i, token);
      i++;
   }
+  printf("end tokens\n");
   
   char *file_name = v[0];
 
@@ -97,22 +112,27 @@ start_process (void *file_name_)
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
   success = load (file_name, &if_.eip, &if_.esp);
-
+  printf("success: %d\n", success);
+  
   asm volatile ("pushl $0":::"memory");
+  printf("push 0\n");
 
   for(i = c-1; i > 0; i--) //put the file name on stack?
   {
     // PUSH v[i]
     asm volatile ("pushl %[arg]"::[arg] "m" (v[i]):"memory");
+    printf("push %s\n", v[i]);
   }
   //int number = 4;
   //asm volatile ("addl %%esp, %[arg]; pushl %[arg]":"=r" (number):[arg] "i" (number):"memory");
 
 
   asm volatile ("movl $4, %%eax; addl %%esp, %%eax; pushl %%eax":::"%eax");
+  printf("push es+4\n");
   
   int numArgs = c-1;
   asm volatile ("pushl %[arg]"::[arg] "m" (numArgs):"memory");
+  printf("push c: %d\n", numArgs);
   
   free(v[0]); //File name
   free(v); //array of pointers to arguments
@@ -123,6 +143,8 @@ start_process (void *file_name_)
   {
     thread_exit ();
   }
+
+  printf("jumping...\n");
 
   /* Start the user process by simulating a return from an
      interrupt, implemented by intr_exit (in
