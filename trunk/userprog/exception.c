860a882c6bf189a8dc3c09aa08f8f7ef4eea3410
09SethT@gmail.com <09SethT@gmail.com@e7c1a69f-48f6-0573-76fe-9e04cafba95b>
2011-11-04 21:12:52 +0000

    Changed page fault's printf and kill to sys_exit. All tests pass! --Seth TODO work on variable naming accuracy.

--- trunk/userprog/exception.c
+++ trunk/userprog/exception.c
@@ -165,12 +165,13 @@ page_fault (struct intr_frame *f)
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-  printf ("Page fault in user at %p: %s error %s page in %s context.\n",
+  /*printf ("Page fault in user at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
-          user ? "user" : "kernel");
-  kill (f);
+          user ? "user" : "kernel");*/
+  //kill (f);
+  sys_exit(-1);
   }
 }
 
